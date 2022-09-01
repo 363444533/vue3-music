e964b1c0445f49a73cea4f365c6803c8f6f5c4ee
曹磊 <81024514+363444533@users.noreply.github.com>
2022-09-01 12:27:02 +0800

    Delete vue3-music directory

--- vue3-music/src/api/ranking.js
+++
@@ -1,7 +0,0 @@
-import request from "@/untils/axios"
-export function getRankingList(){
-    return request({
-        url:'/toplist',
-        method:'get',
-    })
-}
--- vue3-music/src/icons/index.js
+++
@@ -1,5 +0,0 @@
-const request = require.context("./svg",false, /\.svg$/)
-
-request.keys().forEach(src => {
-    request(src)
-})
